5f6673623dd59e4c897bdd412bf42c05041153fe
David McCormick <d.mccormick@wellcome.ac.uk>
2018-01-12 18:11:31 +0000

    Move social to footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -2,6 +2,10 @@
   <div class="footer">
     <div class="container">
       <p>&copy; Beryl Thyer Memorial Africa Trust. Registered Charity Number 1112603.</p>
+      <div class="footer__social">
+        <a class="footer__link" href="#">twitter</a>
+        <a class="footer__link" href="#">facebook</a>
+      </div>
     </div>
   </div>
 </template>
@@ -12,11 +16,39 @@ export default {
 };
 </script>
 
-<style>
-  .footer {
-    font-family: 'proxima-soft';
-    font-size: 1rem;
-    padding: 50px 0;
-    border-top: 1px solid #eee;
+<style lang="scss">
+.footer {
+  font-family: 'proxima-soft';
+  font-size: 1rem;
+  padding: 50px 0;
+  border-top: 1px solid #eee;
+}
+
+.footer__social {
+  margin-top: 1em;
+}
+
+.footer__link {
+  text-decoration: none;
+  color: #47b784;
+  margin-right: 1em;
+  position: relative;
+
+  &:after {
+    position: absolute;
+    content: '';
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    background: #47b784;
+    transition: width 600ms ease;
   }
+
+  &:hover {
+    &:after {
+      width: 100%;
+    }
+  }
+}
 </style>
--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,16 +1,14 @@
 <template>
   <header class="site-header">
-    <div class="container">
-      <div class="site-header__inner">
+    <div class="site-header__inner">
+      <div class="container">
         <h1 class="site-header__title">
           BTMAT
           <span class="site-header__subtitle">Beryl Thyer Memorial Africa Trust, caring for African children with cancer.</span>
         </h1>
-        <div class="site-header__social">
-          <a class="social twitter" href="#"><Icon class="social-icon" name="twitter" /></a>
-          <a class="social facebook" href="#"><Icon class="social-icon" name="facebook" /></a>
-        </div>
       </div>
+    </div>
+    <div class="container">
       <SiteNav />
     </div>
   </header>
@@ -35,6 +33,7 @@ export default {
   background: white;
   font-family: "proxima-soft";
   font-weight: bold;
+  padding-top: 10px;
 
   @media(min-width: 680px) {
     padding-top: 20px;
@@ -71,8 +70,7 @@ export default {
 }
 
 .site-header__inner {
-  display: flex;
-  justify-content: space-between;
+  border-bottom: 3px solid #47b784;
 }
 
 .site-header__title {
@@ -89,40 +87,6 @@ export default {
   display: block;
   margin-top: 0.5em;
   margin-bottom: 1em;
-  padding-right: 60px;
-  color: #aaa;
-}
-
-.site-header__social {
-  position: absolute;
-  right: 20px;
-  top: 55px;
-
-  @media (min-width: 680px) {
-    position: static;
-  }
-}
-
-.twitter {
-  margin-right: 0.2em;
-}
-
-.facebook {
-  position: relative;
-  top: -0.1em;
-}
-
-.icon.social-icon svg {
-  height: 1.5em;
-  fill: #aaa;
-  transition: all 600ms ease;
-
-  .social:hover & {
-    fill: #333;
-  }
-}
-
-.facebook .social-icon svg {
-  height: 1.4em;
+  color: #555;
 }
 </style>
