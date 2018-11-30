907e917f2342fc940d75f8eddc6d23fc3344c164
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-30 19:16:13 +0000

    Fix month/pageSlug with index

--- pages/_year/index.vue
+++ pages/_year/index.vue
@@ -3,29 +3,29 @@
 </template>
 
 <script>
-  import NewsList from '~/components/NewsList';
+import NewsList from '~/components/NewsList';
 export default {
   components: {
-      NewsList
+    NewsList
   },
   beforeRouteUpdate(to, from, next) {
-      this.$store.commit('setCurrentPage', 1);
-      next();
+    this.$store.commit('setCurrentPage', 1);
+    next();
   },
-    async asyncData({ params, store, error }) {
-      const nextYear = Number(params.year) + 1;
-      const after = `${params.year}-01-01T00:00:00`;
-      const before = `${nextYear}-01-01T00:00:00`;
-      try {
-        await store.dispatch('getArticleStubs', {query: {before, after}});
-        return {
-          articleStubs: store.state.articleStubs
-        };
-      } catch (err) {
-        const maybeStatus = err.response && err.response.status;
-        const status = maybeStatus || 404;
-        error({ statusCode: status });
-      }
+  async asyncData({ params, store, error }) {
+    const nextYear = Number(params.year) + 1;
+    const after = `${params.year}-01-01T00:00:00`;
+    const before = `${nextYear}-01-01T00:00:00`;
+    try {
+      await store.dispatch('getArticleStubs', {query: {before, after}});
+      return {
+        articleStubs: store.state.articleStubs
+      };
+    } catch (err) {
+      const maybeStatus = err.response && err.response.status;
+      const status = maybeStatus || 404;
+      error({ statusCode: status });
+    }
   },
   scrollToTop: true
 };
