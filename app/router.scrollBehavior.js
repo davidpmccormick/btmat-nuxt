512286c44a8e31d13f2429b0f82b10c0fbfcda78
David McCormick <davidpmccormick@gmail.com>
2020-08-27 19:44:11 +0100

    Don't scroll past nav if you don't have to

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,19 @@
+export default function(to, from, savedPosition) {
+  const DESKTOP_NAV_HEIGHT = 81;
+  const MAX_MOBILE_NAV = 790;
+  const scrollPosition = window.pageYOffset;
+  const viewportWidth = window.innerWidth;
+
+  return new Promise((resolve, reject) => {
+    this.app.$once('triggerScroll', () => {
+      resolve({
+        x: 0,
+        y:
+          scrollPosition >= DESKTOP_NAV_HEIGHT &&
+          viewportWidth >= MAX_MOBILE_NAV
+            ? DESKTOP_NAV_HEIGHT
+            : scrollPosition
+      });
+    });
+  });
+}
