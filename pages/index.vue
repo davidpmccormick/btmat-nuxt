88d283e4a54f7b5534e0ffe8e15bdcbe7fba08f0
David McCormick <davidpmccormick@gmail.com>
2021-12-07 21:50:51 +0000

    Add donate banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
+    <Banner>
+      <p>Peter McCormick passed away peacefully and surrounded by family on November 26th 2021. Donations in his memory for BTMAT may be made via the <a href="https://www.warwick-funerals.co.uk/donations/">Jack Warwick Funeral Service website</a>.</p>
+    </Banner>
+
     <Cards :news-article="firstArticle" />
   </div>
 </template>
 <script>
+import Banner from '~/components/Banner';
 import Cards from '~/components/Cards';
+
 
 export default {
   head: {
     titleTemplate: '%s | Home'
   },
   components: {
+    Banner,
     Cards
   },
   async asyncData({ store, error }) {
